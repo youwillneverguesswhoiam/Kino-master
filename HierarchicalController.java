7760cebe3a08a65ffe983bf7158b4964a919d6fe
radziq1302 <radziq1302@gmail.com>
2018-06-23 21:55:02 +0200

    Delete HierarchicalController.java

--- HierarchicalController.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public interface HierarchicalController <P extends HierarchicalController<?>> {
-
-    public P getParentController();
-    public void setParentController(P parent);
-
-    //coś tu nie działa
-}
